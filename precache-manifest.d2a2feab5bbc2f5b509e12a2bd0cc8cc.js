66acde3419233eced7dfaade859f02a46147858b
Mario Lema <lema_mario@yahoo.com>
2019-02-21 17:35:43 +0000

    Updates

---
+++ precache-manifest.d2a2feab5bbc2f5b509e12a2bd0cc8cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1baafa02b198301cadece990f0412f48",
+    "url": "/react-calculator/static/media/mountains.1baafa02.jpeg"
+  },
+  {
+    "revision": "a2ecf2e68de309a6bd2b",
+    "url": "/react-calculator/static/js/runtime~main.a2ecf2e6.js"
+  },
+  {
+    "revision": "49bb1d3b3cb925446472",
+    "url": "/react-calculator/static/js/main.49bb1d3b.chunk.js"
+  },
+  {
+    "revision": "27b8461e207d4a36e491",
+    "url": "/react-calculator/static/js/1.27b8461e.chunk.js"
+  },
+  {
+    "revision": "49bb1d3b3cb925446472",
+    "url": "/react-calculator/static/css/main.9aebf679.chunk.css"
+  },
+  {
+    "revision": "c286e33b956b879d2afb04be727e8015",
+    "url": "/react-calculator/index.html"
+  }
+];
